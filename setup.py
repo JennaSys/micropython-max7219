5a678065fead995724242ba198b5fa769ffdbffe
John Sheehan <jennasyseng@mail.com>
2019-05-21 01:33:36 -0700

    Added setup (not tested!)

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+import sdist_upip
+
+setup(name='micropython-max7219',
+      version='0.1.0',
+      description='MicroPython driver for MAX7219 with 7-segment modules',
+      long_description=open('README').read(),
+      url='https://github.com/JennaSys/micropython-max7219',
+      author='John Sheehan',
+      author_email='jennasyseng@gmail.com',
+      licence='MIT',
+      cmdclass={'sdist': sdist_upip.sdist},
+      py_modules=['max7219', 'seven_segment_ascii']
+)
